6bb0b98236ebc5c8c3f55b8e6d1b585a8b4d6c15
umeshstar100 <umeshstar9410@gmail.com>
2024-08-13 15:44:45 +0530

    first commit

---
+++ file1.js
@@ -0,0 +1,122 @@
+// let a=10;
+// document.write(a, "<br>");
+// let b=5;
+// document.write(b,"<br>");
+// let sum =a+b;
+// document.write(sum, "<br>");
+// let difference = a-b;
+// document.write(difference, "<br>");
+// let product = a*b;
+// document.write(product, "<br>");
+// let quotient=a/b;
+// document.write(quotient, "<br>");
+// let remainder = a%b;
+// document.write(remainder, "<br>");
+// let isEqual= (a==b);
+// document.write(isEqual, "<br>");
+// let isNotEqual=(a != b);
+// document.write(isNotEqual, "<br>");
+// let isGreater=(a>b);
+// document.write(isGreater, "<br>");
+// let logicalAnd=(a>b && b<10);
+// document.write(logicalAnd, "<br>");
+
+// let fname="John";
+// document.write("Hello", fname, "<br>");
+
+
+// let age=30;
+// document.write('Age',age,"<br>")
+
+// let isStudent = false;
+// document.write(`student - ${isStudent? "yes":"No"},<br>`)
+
+
+// let age=30;
+// document.write('age', age,"<br>")
+
+// let age=30;
+// let emptyValue=null;
+// let unintialized;
+
+
+// let age=16;
+// if(age>17){
+//     document.write("Eligible for vote");
+// }
+// else{
+//     document.write("Not eligible for vote");
+
+// let age=16;
+// document.write(age>17? "Eligible": "Not eligible");
+
+// let number=prompt("Enter number");
+
+// if(number>0){
+//     console.log("The number is positive");
+
+// }
+// else if( number <0){
+//     console.log("The number is negtive");
+
+// }
+// else{
+//     console.log("the number is zero");
+
+// }
+
+// confirm("Enter again") && goto again;
+
+// //-----------------------------------
+// let fruit=prompt("Enter fruit name");
+// switch(fruit) {
+//     case "Apple":
+//         console.log("You choose a apple");
+        
+// }
+
+// switch(fruit){
+//     case: 
+// }
+
+//simple function
+function morning(){
+    document.getElementById("demo").innerHTML="Good morning"
+    document.getElementById("mode").style.background="Yellow"
+    document.getElementById("mode").style.color="black"
+}
+function afternoon(){
+    document.getElementById("demo").innerHTML="Good afternoon"
+    document.getElementById("mode").style.background="orange"
+    document.getElementById("mode").style.color="orange"
+}
+function evening(){
+    document.getElementById("demo").innerHTML="Good evening"
+    document.getElementById("mode").style.background="lightyellow"
+    document.getElementById("mode").style.color="lightyellow"
+}
+function night(){
+    document.getElementById("demo").innerHTML="Good night"
+    document.getElementById("mode").style.background="Black"
+    document.getElementById("mode").style.color="Black"
+}
+// greeting();                  //function calling
+
+
+//function expression
+// let a=function(){
+//     document.write("Have a nice day")
+// }
+// a()
+
+//arrow function
+// let msg=()=>{
+//     document.write("Happy Birthday")
+// }
+// msg()
+
+//iife
+// (function(){
+//     document.write("This is IIFE function")
+// }()
+// );
